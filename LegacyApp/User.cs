94ab992fa9345a0723b1555aefd9ba69d80231fa
Arkhi Arkhi <cannavaro-realm@yandex.ru>
2021-11-11 14:46:01 +0500

    Отрефакторил код в соответствии с заданием

--- LegacyApp/User.cs
+++ LegacyApp/User.cs
@@ -5,11 +5,47 @@ namespace LegacyApp
     public class User
     {
         public Client Client { get; set; }
+
         public DateTime DateOfBirth { get; set; }
+
         public string EmailAddress { get; set; }
+
         public string FirstName { get; set; }
+
         public string Surname { get; set; }
-        public int CreditLimit { get; set; }
-        public bool HasCreditLimit { get; set; }
+
+        public int CreditLimit => CalculateCreditLimit();
+
+        public bool HasCreditLimit => !Client.IsVeryImportantClient();
+
+        public bool IsResponsible()
+        {
+            return IsAdult() && ((HasCreditLimit && CreditLimit >= 500) || Client.IsVeryImportantClient());
+        }
+
+        private bool IsAdult()
+        {
+            return CalculateAge() >= 21;
+        }
+
+        private int CalculateAge()
+        {
+            // Можно описать класс калькулятора возраста (по Single Responsibility Principle) и вынести в него этот метод!
+            // и 365 дней в году вынести в константу
+            var span = DateTime.Now.Subtract(DateOfBirth);
+            return (int)span.TotalDays / 365;
+        }
+
+        private int CalculateCreditLimit()
+        {
+            if (!HasCreditLimit)
+                return 0;
+
+            using (var userCreditService = new UserCreditServiceClient())
+            {
+                var creditLimit = userCreditService.GetCreditLimit(FirstName, Surname, DateOfBirth);
+                return Client.IsImportantClient() ? 2 * creditLimit : creditLimit;
+            }  
+        }
     }
 }
