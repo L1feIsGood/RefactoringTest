840741027ddcd5362ec98d5849d508d1d9f59457
RasilZiyatdinov <65215767+RasilZiyatdinov@users.noreply.github.com>
2022-01-20 20:39:40 +0500

    Update User.cs

--- LegacyApp/User.cs
+++ LegacyApp/User.cs
@@ -1,9 +1,19 @@
-﻿using System;
+using System;
 
 namespace LegacyApp
 {
     public class User
     {
+        public User(Client client, DateTime dateOfBirth, string emailAddress, string firstName, string surname, int creditLimit = 0, bool hasCreditLimit = false)
+        {
+            Client = client;
+            DateOfBirth = dateOfBirth;
+            EmailAddress = emailAddress;
+            FirstName = firstName;
+            Surname = surname;
+            CreditLimit = creditLimit;
+            HasCreditLimit = hasCreditLimit;
+        }
         public Client Client { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string EmailAddress { get; set; }
