df771dbd906028037d1ff8f54a059cf28b3e7e64
RasilZiyatdinov <65215767+RasilZiyatdinov@users.noreply.github.com>
2022-01-20 20:28:13 +0500

    Update UserService.cs

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,42 +1,48 @@
-﻿using System;
+using System;
 
 namespace LegacyApp
 {
     public class UserService
     {
+   
         public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
-            {
-                return false;
-            }
+            if (CheckName(firName, surname)) return false;
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
+            if (CheckEmail(email)) return false;
 
+            if (CheckAge(dateOfBirth, 21)) return false;
+
+            ClientRepository clientRepository = new ClientRepository();
+            Client client = clientRepository.GetById(clientId);
+
+            User user = new User(client, dateOfBirth, email, firName, surname);
+
+            SetCreditLimit(client, ref user);
+
+            if (CheckCreditLimit(user, 500)) return false;
+
+            UserDataAccess.AddUser(user);
+
+            return true;
+        }
+        private bool CheckAge(DateTime dateOfBirth, int checkingAge)
+        {
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
-
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
-
-            var user = new User
-            {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firName,
-                Surname = surname
-            };
-
+            return age < checkingAge;
+        }
+        private bool CheckName(string firName, string surname)
+        {
+            return (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname));
+        }
+        private bool CheckEmail(string email)
+        {
+            return (!email.Contains("@") && !email.Contains("."));
+        }
+        private void SetCreditLimit(Client client, ref User user)
+        {
             if (client.Name == "VeryImportantClient")
             {
                 // Пропустить проверку лимита
@@ -63,15 +69,10 @@ namespace LegacyApp
                     user.CreditLimit = creditLimit;
                 }
             }
-
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
-
-            UserDataAccess.AddUser(user);
-
-            return true;
+        }
+        private bool CheckCreditLimit(User user, int checkingLimit)
+        {
+            return (user.HasCreditLimit && user.CreditLimit < checkingLimit);
         }
     }
 }
