04f6e93ed0911dde008aed1180678a4183bbc385
Yaroslaw <bruce.levitangens@gmail.com>
2022-04-13 18:33:44 +0500

    Add files via upload

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -4,26 +4,14 @@ namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
+        public bool AddUser(string firName,
+            string surName,
+            string email,
+            DateTime dateOfBirth,
+            int clientId)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
-            {
+            if (!ValidateNewUser.Validate(firName, surName, email, dateOfBirth))
                 return false;
-            }
-
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
@@ -34,44 +22,46 @@ namespace LegacyApp
                 DateOfBirth = dateOfBirth,
                 EmailAddress = email,
                 FirstName = firName,
-                Surname = surname
+                Surname = surName
             };
 
-            if (client.Name == "VeryImportantClient")
-            {
-                // Пропустить проверку лимита
-                user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Проверить лимит и удвоить его
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                // Проверить лимит
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            UpdateCreditLimit(ref user);
+
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
                 return false;
-            }
 
             UserDataAccess.AddUser(user);
 
             return true;
+        }
+
+        private static void UpdateCreditLimit(ref User user)
+        {
+            int limitMultiplier = 1;
+            switch (user.Client.Name)
+            {
+                // Пропустить проверку лимита
+                case "VeryImportantClient":
+                    user.HasCreditLimit = false;
+                    return;
+                // Проверить лимит и удвоить его
+                case "ImportantClient":
+                    user.HasCreditLimit = true;
+                    limitMultiplier =  2;
+                    break;
+                // Проверить лимит
+                default:
+                    user.HasCreditLimit = true;
+                    break;
+            }
+
+            using var userCreditService = new UserCreditServiceClient();
+            int creditLimit = userCreditService.
+                GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
+
+            creditLimit *= limitMultiplier;
+            user.CreditLimit = creditLimit;
         }
     }
 }
---
+++ LegacyApp/ValidateNewUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LegacyApp;
+
+public static class ValidateNewUser
+{
+    public static bool Validate(
+            string firstName,
+            string surname,
+            string email,
+            DateTime dateOfBirth)
+    {
+        if (!CheckingUserName(firstName, surname)
+            || !CheckingEmail(email)
+            || GetAge(dateOfBirth) < 21)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static bool CheckingUserName(string firstName, string surname)
+        => !(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(surname));
+
+    private static bool CheckingEmail(string email)
+        => email.Contains("@") && email.Contains(".");
+
+    private static int GetAge(DateTime dateOfBirth)
+    {
+        var now = DateTime.Now;
+        int age = now.Year - dateOfBirth.Year;
+
+        if (now.Month < dateOfBirth.Month
+            || (now.Month == dateOfBirth.Month
+                && now.Day < dateOfBirth.Day)
+            )
+        {
+            age--;
+        }
+
+        return age;
+    }
+}
+
