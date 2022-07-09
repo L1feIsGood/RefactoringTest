18bc4b3518dc4415c1b5e4f089c3220349b065a6
Andrey <catsboom@mail.ru>
2022-07-09 15:10:24 +0500

    Исправления и дополнения UserService

---
+++ LegacyApp/UserFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyApp
+{
+    class UserFactory
+    {
+        public User CreateUser(int clientId, DateTime dateOfBirth, string email, string firstName,string surName)
+        {
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            User user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                Surname = surName
+            };
+
+            switch (client.Name)
+            {
+                case "VeryImportantClient":
+                    {
+                        // Пропустить проверку лимита
+                        user.HasCreditLimit = false;
+                    }
+                    break;
+                case "ImportantClient":
+                    {
+                        // Проверить лимит и удвоить его
+                        user.HasCreditLimit = true;
+                        using (var userCreditService = new UserCreditServiceClient())
+                        {
+                            var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
+                            creditLimit *= 2;
+                            user.CreditLimit = creditLimit;
+                        }
+                    }
+                    break;
+                default:
+                    {
+                        // Проверить лимит
+                        user.HasCreditLimit = true;
+                        using (var userCreditService = new UserCreditServiceClient())
+                        {
+                            var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
+                            user.CreditLimit = creditLimit;
+                        }
+                    }
+                    break;
+            }
+
+            return user;
+        }
+
+        public enum ClientImportant
+        {
+            VeryImportantClient,
+            ImportantClient
+        }
+
+    }
+}
--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -6,65 +6,28 @@ namespace LegacyApp
     {
         public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
         {
-            var now_time = DateTime.Now;
-            int age_user = now_time.Year - dateOfBirth.Year;
-            if (now_time.Month < dateOfBirth.Month || (now_time.Month == dateOfBirth.Month && now_time.Day < dateOfBirth.Day)) age_user--;
+            UserFactory user_factory = new UserFactory();
+
+            DateTime now_time = DateTime.Now;
+            int age_user = (int)((now_time - dateOfBirth).TotalDays / 365);            
 
             if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname) || age_user < 21 || !email.Contains("@") && !email.Contains("."))
             {
                 return false;
-            }
+            }            
 
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
-            
-            var user = new User
-            {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firName,
-                Surname = surname
-            };
-            
-            switch (client.Name)
-            {
-                case "VeryImportantClient":
-                {
-                    // Пропустить проверку лимита
-                    user.HasCreditLimit = false;
-                }break;
-                case "ImportantClient":
-                {
-                        // Проверить лимит и удвоить его
-                    user.HasCreditLimit = true;
-                    using (var userCreditService = new UserCreditServiceClient())
-                    {
-                        var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                        creditLimit *= 2;
-                        user.CreditLimit = creditLimit;
-                    }
-                }break;
-                default:
-                {
-                    // Проверить лимит
-                    user.HasCreditLimit = true;
-                    using (var userCreditService = new UserCreditServiceClient())
-                    {
-                        var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                        user.CreditLimit = creditLimit;
-                    }
-                }break;
-            }
+            User new_user = user_factory.CreateUser(clientId, dateOfBirth, email, firName, surname);
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
+            if (new_user.HasCreditLimit && new_user.CreditLimit < 500)
             {
                 return false;
             }
 
-            UserDataAccess.AddUser(user);
+            UserDataAccess.AddUser(new_user);
 
             return true;
         }
     }
+
+    
 }
