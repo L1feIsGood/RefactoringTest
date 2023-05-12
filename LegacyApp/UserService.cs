4f46982122d6b198109beaf87c50c53810d8aa9d
maloun <justthesimpleuser@gmail.com>
2023-05-12 16:01:11 +0500

    Провёл рефакторинг метода UserService.AddUser

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,77 +1,137 @@
 ﻿using System;
+using System.Security.Policy;
 
 namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
+		public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
+		{
+			try
+			{
+				if (!IsUserInputValid(firName, surname, email, dateOfBirth))
+				{
+					return false;
+				}
+
+				var clientRepository = new ClientRepository();
+				var client = clientRepository.GetById(clientId);
+				var user = CreateUser(client, firName, surname, email, dateOfBirth);
+				ValidateAndSetUserCreditLimit(ref user, client);
+
+				UserDataAccess.AddUser(user);
+
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public bool IsUserInputValid(string firName, string surname, string email, DateTime dateOfBirth)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
+					return false;
+
+				if (!email.Contains("@") && !email.Contains("."))
+					return false;
+
+				if (GetUserAge(dateOfBirth) < 21)
+					return false;
+
+				return true;
+			}
+			catch (Exception e)
+			{
+				throw new Exception($"Исключение в UserService.IsUserInputValid {e.Message}");
+			}
+		}
+
+		public int GetUserAge(DateTime dateOfBirth)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
+            try
             {
-                return false;
+                var now = DateTime.Now;
+                int age = now.Year - dateOfBirth.Year;
+                DateTime thisYearBirthday = dateOfBirth.AddYears(age);
+
+				if (thisYearBirthday > now)
+                    age--;
+
+                return age;
             }
-
-            if (!email.Contains("@") && !email.Contains("."))
+            catch (Exception e)
             {
-                return false;
+                throw new Exception($"Исключение в UserService.GetUserAge {e.Message}");
             }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
-
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
-
-            var user = new User
-            {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firName,
-                Surname = surname
-            };
-
-            if (client.Name == "VeryImportantClient")
-            {
-                // Пропустить проверку лимита
-                user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Проверить лимит и удвоить его
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                // Проверить лимит
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
-
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
-
-            UserDataAccess.AddUser(user);
-
-            return true;
         }
+
+		public User CreateUser(Client client, string firName, string surname, string email, DateTime dateOfBirth)
+		{
+			try
+			{
+				var user = new User
+				{
+					Client = client,
+					DateOfBirth = dateOfBirth,
+					EmailAddress = email,
+					FirstName = firName,
+					Surname = surname
+				};
+				return user;
+			}
+			catch (Exception e)
+			{
+				throw new Exception($"Исключение в UserService.CreateUser {e.Message}");
+			}
+		}
+
+		public bool ValidateAndSetUserCreditLimit(ref User user, Client client)
+		{
+			try
+			{
+				if (client.Name == "VeryImportantClient")
+					// Пропустить проверку лимита
+					user.HasCreditLimit = false;
+				else if (client.Name == "ImportantClient")
+					// Проверить лимит и удвоить его
+					SetCreditLimit(ref user, true);
+				else
+					// Проверить лимит 
+					SetCreditLimit(ref user, false);
+
+				var result = user.HasCreditLimit && user.CreditLimit < 500;
+				return result;
+			}
+			catch (Exception e)
+			{
+				throw new Exception($"Исключение в UserService.ValidateAndSetUserCreditLimit {e.Message}");				
+			}
+		}
+
+		public void SetCreditLimit(ref User user, bool IncreaseLimitX2)
+		{
+			try 
+			{ 
+				user.HasCreditLimit = true;
+				using (var userCreditService = new UserCreditServiceClient())
+				{
+					var creditLimit = userCreditService.GetCreditLimit(
+						user.FirstName, user.Surname, user.DateOfBirth);
+
+					if (IncreaseLimitX2) 
+						creditLimit *= 2;
+
+					user.CreditLimit = creditLimit;
+				}
+			}
+			catch (Exception e)
+			{
+				throw new Exception($"Исключение в UserService.SetUserLimit {e.Message}");
+			}
+		}
     }
 }
