187eeda2bad965843859d7a44d31cf48bbadb07b
K R <kr.perm.ru@gmail.com>
2023-06-08 17:51:31 +0500

    рефакторинг

--- LegacyApp/Client.cs
+++ LegacyApp/Client.cs
@@ -2,6 +2,9 @@
 {
     public class Client
     {
+        public const string VeryImportantClientName = "VeryImportantClient";
+        public const string ImportantClientName = "ImportantClient";
+
         public int Id { get; set; }
         public string Name { get; set; }
         public ClientStatus ClientStatus { get; set; }
--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,29 +1,25 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
+        public const int MinAllowedUserAge = 21;
+        public const int MinAllowedCreditLimit = 500;
+        public const int ImportantClientCreditLimitMultiplier = 2;
+
+        public bool AddUser(string firstName, string surname, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
-            {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(surname) || string.IsNullOrEmpty(email))
                 return false;
-            }
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
                 return false;
-            }
 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
+            var minAllowedDateOfBirth = DateTime.Now.AddYears(-MinAllowedUserAge);
+            if (dateOfBirth < minAllowedDateOfBirth)
                 return false;
-            }
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
@@ -33,44 +29,29 @@ namespace LegacyApp
                 Client = client,
                 DateOfBirth = dateOfBirth,
                 EmailAddress = email,
-                FirstName = firName,
+                FirstName = firstName,
                 Surname = surname
             };
 
-            if (client.Name == "VeryImportantClient")
+            if (client.Name == Client.VeryImportantClientName)
             {
                 // Пропустить проверку лимита
                 user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Проверить лимит и удвоить его
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                // Проверить лимит
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
+                UserDataAccess.AddUser(user);
+                return true;
             }
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
+            // Проверить лимит
+            user.HasCreditLimit = true;
+            using (var userCreditService = new UserCreditServiceClient())
+                user.CreditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
+            // Удвоить лимит, если важный клиент
+            if (client.Name == Client.ImportantClientName)
+                user.CreditLimit *= ImportantClientCreditLimitMultiplier;
+            if (user.CreditLimit < MinAllowedCreditLimit)
                 return false;
-            }
 
             UserDataAccess.AddUser(user);
-
             return true;
         }
     }
