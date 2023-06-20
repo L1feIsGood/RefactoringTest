221da4036a073fb0636d7d01a18899fce2ecfebb
Evil-maker <carsackov.denis@ya.ru>
2023-06-20 21:27:13 +0300

    Рефакторинг Карсаков Д.

---
+++ LegacyApp/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyApp.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetTodayAge(this DateTime date)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - date.Year;
+
+            if (now.Month < date.Month || (now.Month == date.Month && now.Day < date.Day))
+                age--;
+
+            return age;
+        }
+    }
+}
--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LegacyApp.Extensions;
+using System;
 
 namespace LegacyApp
 {
@@ -6,27 +7,10 @@ namespace LegacyApp
     {
         public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
-            {
+            if (!IsValidInput(firName, surname, email, dateOfBirth))
                 return false;
-            }
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
-
-            var clientRepository = new ClientRepository();
-            var client = clientRepository.GetById(clientId);
+            var client = GetClientById(clientId);
 
             var user = new User
             {
@@ -37,39 +21,62 @@ namespace LegacyApp
                 Surname = surname
             };
 
-            if (client.Name == "VeryImportantClient")
+            var creditLimit = GetUserCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
+
+            switch (client.Name)
             {
-                // Пропустить проверку лимита
-                user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Проверить лимит и удвоить его
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
+                case "VeryImportantClient":
+                    user.HasCreditLimit = false;
+                    break;
+
+                case "ImportantClient":
+                    user.HasCreditLimit = true;
+                    user.CreditLimit = creditLimit * 2;
+                    break;
+
+                default:
+                    user.HasCreditLimit = true;
                     user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                // Проверить лимит
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
+                    break;
             }
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
                 return false;
-            }
 
             UserDataAccess.AddUser(user);
+
+            return true;
+        }
+
+        private Client GetClientById(int clientId)
+        {
+            var clientRepository = new ClientRepository();
+            return clientRepository.GetById(clientId);
+        }
+
+        private int GetUserCreditLimit(string firstName, string surname, DateTime dateOfBirth)
+        {
+            using (var userCreditService = new UserCreditServiceClient())
+            {
+                var creditLimit = userCreditService.GetCreditLimit(firstName, surname, dateOfBirth);
+                return creditLimit;
+            }
+        }
+
+        private bool IsValidInput(string firName, string surname, string email, DateTime dateOfBirth)
+        {
+            if (string.IsNullOrEmpty(surname))
+                return false;
+
+            if (string.IsNullOrEmpty(firName))
+                return false;
+
+            if (!email.Contains("@") && !email.Contains("."))
+                return false;
+
+            var age = dateOfBirth.GetTodayAge();
+            if (age < 21)
+                return false;
 
             return true;
         }
