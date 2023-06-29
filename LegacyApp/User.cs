5e0f16b2d5ea219ec58f698002551b1bf05d2b35
fully <roms220@yandex.ru>
2023-06-29 17:53:46 +0500

    Вынес валидаторы в отдельные функции. Вынес работу с CreditLimit в модель User.

--- LegacyApp/User.cs
+++ LegacyApp/User.cs
@@ -11,5 +11,40 @@ namespace LegacyApp
         public string Surname { get; set; }
         public int CreditLimit { get; set; }
         public bool HasCreditLimit { get; set; }
+
+        public void CheckCreditLimit()
+        {
+            HasCreditLimit = true;
+            using (var userCreditService = new UserCreditServiceClient())
+            {
+                var creditLimit = userCreditService.GetCreditLimit(FirstName, Surname, DateOfBirth);
+                CreditLimit = creditLimit;
+            }
+        }
+
+        public void IncreaseLimit(int amount = 1)
+        {
+            HasCreditLimit = true;
+            using (var userCreditService = new UserCreditServiceClient())
+            {
+                var creditLimit = userCreditService.GetCreditLimit(FirstName, Surname, DateOfBirth);
+                creditLimit = creditLimit * amount;
+                CreditLimit = creditLimit;
+            }
+        }
+
+        public void SetLimit()
+        {
+            if (Client.Name == "VeryImportantClient")
+                // Пропустить проверку лимита
+                HasCreditLimit = false;
+
+            else if (Client.Name == "ImportantClient")
+                // Проверить лимит и удвоить его
+                IncreaseLimit(2);
+            else
+                // Проверить лимит
+                CheckCreditLimit();
+        }
     }
 }
--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -6,24 +6,11 @@ namespace LegacyApp
     {
         public bool AddUser(string firName, string surname, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
-            {
+            if (!ValidateFields(firName, surname, email))
                 return false;
-            }
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
+            if (!ValidateAge(dateOfBirth))
                 return false;
-            }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
@@ -37,39 +24,37 @@ namespace LegacyApp
                 Surname = surname
             };
 
-            if (client.Name == "VeryImportantClient")
-            {
-                // Пропустить проверку лимита
-                user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Проверить лимит и удвоить его
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                // Проверить лимит
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.FirstName, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            user.SetLimit();
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
                 return false;
-            }
 
             UserDataAccess.AddUser(user);
+
+            return true;
+        }
+
+        private bool ValidateFields(string firName, string surname, string email)
+        {
+            if (string.IsNullOrEmpty(firName) || string.IsNullOrEmpty(surname))
+                return false;
+
+            if (!email.Contains("@") && !email.Contains("."))
+                return false;
+
+            return true;
+        }
+
+        private bool ValidateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+                age--;
+
+            if (age < 21)
+                return false;
 
             return true;
         }
